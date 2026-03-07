fa58b3ca188dd0e125a96a791f4e969f3d825017
AlexD2006-dev <lucia.draesner@code.berlin>
2026-03-07 19:24:07 +0100

    Added about and projecs routes

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ app.get('/', (request, response) => {
   response.send('Hello! Welcome to Alex Draesner\'s Portfolio! 🎉')
 })
 
+app.get('/about', (request, response) => {
+  response.send('This is Alex Draesner\'s About Page!')
+})
+
+app.get('/projects', (request, response) => {
+  response.send('This is Alex Draesner\'s Projects Page!')
+})
+
 app.listen(PORT, () => {
     console.log(`Started server on port ${PORT}`)
 })
