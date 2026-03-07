87753dc1b06bc63d692d9a73da7997b6d2610aaf
AlexD2006-dev <lucia.draesner@code.berlin>
2026-03-07 19:15:35 +0100

    Added nodemon <nd start scripts

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express()
 const PORT = 3000
 
 app.get('/', (request, response) => {
-  response.send('Welcome to Alex Draesner\'s Portfolio!')
+  response.send('Hello! Welcome to Alex Draesner\'s Portfolio! 🎉')
 })
 
 app.listen(PORT, () => {
