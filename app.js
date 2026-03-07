44b4119db86e8d3cb2f2462429145f9ac38ee9df
AlexD2006-dev <lucia.draesner@code.berlin>
2026-03-07 20:27:17 +0100

    Added dynamic project route

--- app.js
+++ app.js
@@ -15,6 +15,12 @@ app.get('/projects', (request, response) => {
   response.send('This is Alex Draesner\'s Projects Page!')
 })
 
+app.get('/project/:projectName', (request, response) => {
+  const projectName = request.params.projectName
+  const prettyName = projectName.replace(/-/g, ' ')
+  response.send(`<h1>${prettyName}</h1><p>Details about ${prettyName} will go here.</p><a href="/projects">← Back to Projects</a>`)
+})
+
 app.listen(PORT, () => {
     console.log(`Started server on port ${PORT}`)
 })
